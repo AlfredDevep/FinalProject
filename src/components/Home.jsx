f5d2f54062538bd0030e7da9b6fb9048788f6f68
= <=>
2024-07-01 21:12:36 -0600

    cambios al front

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,12 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase/config';
+//import { LoginForm } from './LoginForm';
 
 
 const Home = () => {
   const navigate = useNavigate();
-
+  
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -20,19 +21,22 @@ const Home = () => {
   };
 
   return (
-      <div className="d-flex flex-column align-items-center vh-100">
-        <h1>Bienvenido a la página de inicio</h1>
+    <div className='banner'>
+      <div className="d-flex flex-column align-items-center vh-100" style={{ backgroundImage:"(https://img.freepik.com/foto-gratis/fondo-hiperespacial-3d-efecto-tunel-urdimbre_1048-12526.jpg)"}}>
+        
+        <h1 className='text-warning'>Bienvenido a la página de inicio</h1>
         <button onClick={handleLogout} className="btn btn-danger">Cerrar sesión</button>
         <nav>
           <ul>
             <li><Link to="/planetas">Planetas</Link></li>
             <li><Link to="/peliculas">Peliculas</Link></li>
             <li><Link to="/personajes">Personajes</Link></li>
-          </ul>
+            <li><Link to="../firebase/session">Datos</Link></li>
+            </ul>
         </nav>
 
       </div>
-
+    </div>
   );
 };
 
---
+++ src/firebase/session.jsx
@@ -0,0 +1,20 @@
+
+import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence, sendPasswordResetEmail} from 'firebase/auth'
+
+export const session = () => {
+    const auth = getAuth();
+    setPersistence(auth, browserSessionPersistence)
+    .then(()=>{
+        return signInWithEmailAndPassword(auth, email, password);
+    })
+    .catch((error)=>{
+        const errorCode = error.code;
+        const erroMessage = error.message;
+    });
+    return (
+        <>
+            <label>Email:{{email}}
+            </label>
+        </>
+    )
+}
